6baf1ac91c00b9ec83017ff0b28f06309375cb73
Jérémy Coelho <jcoelho@digitaleo.com>
2014-12-18 11:30:21 +0100

    Classes pour lister les salles

--- public/app/components/map/map.factory.js
+++ public/app/components/map/map.factory.js
@@ -1,29 +1,30 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	angular
-	.module('LaFabriqueApp')
-	.factory('MapFactory', MapFactory);
+    angular
+        .module('LaFabriqueApp')
+        .factory('MapFactory', MapFactory);
 
-	MapFactory.$inject = ['$http'];
+    MapFactory.$inject = ['$http'];
 
-	function MapFactory($http){
-		return {
-			getDesktopGroupsByZone: getDesktopGroupsByZone,
-		}
-		
-		function getDesktopGroupsByZone(zone) {
-			return $http.get('/api/desktop_group/zone/' + zone)
-			.then(complete)
-			.catch(failed);
+    function MapFactory($http) {
+        return {
+            getDesktopGroupsByZone: getDesktopGroupsByZone,
+        }
 
-			function complete(response) {
-				return response.data;
-			}
-			function failed(error) {
-				console.error('Map getDesktopGroupsByZone error: ' + error.data);
-			}
-		};
-	};
+        function getDesktopGroupsByZone(zone) {
+            return $http.get('/api/desktop_group/zone/' + zone)
+                .then(complete)
+                .catch(failed);
+
+            function complete(response) {
+                return response.data;
+            }
+
+            function failed(error) {
+                console.error('Map getDesktopGroupsByZone error: ' + error.data);
+            }
+        };
+    };
 
 })();
